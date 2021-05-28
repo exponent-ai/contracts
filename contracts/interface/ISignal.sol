f17913af2f67e21078fc79d79f98edd6a5856b37
0x1A16A801084B3E97CA000000 <yotta@exponent.ai>
2021-05-28 11:44:30 +0700

    create Signal interface

---
+++ contracts/interface/ISignal.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.8.0;
+
+interface ISignal {
+    function registerSignal(string memory, string memory, string[] memory)
+        external
+        returns (string memory);
+
+    function withdrawSignal(string memory) external;
+
+    function submitSignal(
+        string memory,
+        string[] memory,
+        int256[] memory,
+        bytes calldata
+    ) external;
+
+    function updateSignal(string memory) external;
+
+    function getSignal(string memory) external view returns (int256[] memory);
+
+    function getSignalMeta(string memory) external view returns (string[] memory);
+
+    function getMetaData() external view returns (string memory);
+}
