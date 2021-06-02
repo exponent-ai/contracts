479fbb3b97ebf9b60711b6841c5bf452db139d3f
mega <mega@exponent.ai>
2021-06-02 23:14:11 +0700

    Add new deployment script

---
+++ scripts/deploy-old.js
@@ -0,0 +1,57 @@
+async function main() {
+  const [depositor] = await ethers.getSigners();
+
+  console.log(`Deploying contracts with the account: ${depositor.address}`);
+  console.log("Account balance:", (await depositor.getBalance()).toString());
+
+  // XPNSignalMath
+  const SignalMath = await ethers.getContractFactory("XPNSignalMath");
+  signalMath = await SignalMath.deploy();
+  console.log("XPNSignalMath deployed to:", signalMath.address);
+
+  // XPNSettlement
+  const XPNSettlement = await ethers.getContractFactory("XPNSettlement");
+  tradesettlement = await XPNSettlement.deploy();
+  console.log("XPNSettlement deployed to:", tradesettlement.address);
+
+  // TODO: Change this for and underlying asset address
+  // const MockToken = await ethers.getContractFactory("MockERC20");
+  // token = await MockToken.deploy("DAI token", "DAI");
+  const daiAddress = "0xc4375b7de8af5a38a93548eb8453a498222c4ff2";
+
+  // XPNVault
+  const Vault = await ethers.getContractFactory("XPNVault");
+  vault = await Vault.deploy(daiAddress, "Exponent DAI LP Token", "EXLP-DAI");
+  console.log("XPNVault deployed to:", vault.address);
+
+  // XPNSignalCore
+  const Util = await ethers.getContractFactory("XPNUtils");
+  util = await Util.deploy();
+  console.log("XPNUtils deployed to:", util.address);
+  const SignalFund = await ethers.getContractFactory("XPNSignalCore", {
+    libraries: {
+      XPNUtils: util.address,
+    },
+  });
+  // TODO: Change thsese for and underlying asset address
+  let mockAddress = "0x0000000000000000000000000000000000000000";
+  const assetAddresses = [
+    { name: "BTC", address: mockAddress, priceFeed: mockAddress },
+    { name: "ETH", address: mockAddress, priceFeed: mockAddress },
+    { name: "USDT", address: mockAddress, priceFeed: mockAddress },
+  ];
+  signalFund = await SignalFund.deploy(
+    vault.address,
+    assetAddresses.map((x) => x.name),
+    assetAddresses.map((x) => x.address),
+    assetAddresses.map((x) => x.priceFeed)
+  );
+  console.log("XPNSignalCore deployed to:", signalFund.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,49 +4,70 @@ async function main() {
   console.log(`Deploying contracts with the account: ${depositor.address}`);
   console.log("Account balance:", (await depositor.getBalance()).toString());
 
-  // XPNSignalMath
-  const SignalMath = await ethers.getContractFactory("XPNSignalMath");
-  signalMath = await SignalMath.deploy();
-  console.log("XPNSignalMath deployed to:", signalMath.address);
-
-  // XPNSettlement
-  const XPNSettlement = await ethers.getContractFactory("XPNSettlement");
-  tradesettlement = await XPNSettlement.deploy();
-  console.log("XPNSettlement deployed to:", tradesettlement.address);
-
-  // TODO: Change this for and underlying asset address
-  // const MockToken = await ethers.getContractFactory("MockERC20");
-  // token = await MockToken.deploy("DAI token", "DAI");
-  const daiAddress = "0xc4375b7de8af5a38a93548eb8453a498222c4ff2";
-
-  // XPNVault
-  const Vault = await ethers.getContractFactory("XPNVault");
-  vault = await Vault.deploy(daiAddress, "Exponent DAI LP Token", "EXLP-DAI");
-  console.log("XPNVault deployed to:", vault.address);
-
-  // XPNSignalCore
+  // Deploy XPNUtils
   const Util = await ethers.getContractFactory("XPNUtils");
-  util = await Util.deploy();
-  console.log("XPNUtils deployed to:", util.address);
-  const SignalFund = await ethers.getContractFactory("XPNSignalCore", {
+  this.util = await Util.deploy();
+  await this.util.deployed();
+  console.log("XPNUtils deployed to:", this.util.address);
+
+  const Core = await ethers.getContractFactory("XPNCore", {
     libraries: {
-      XPNUtils: util.address,
+      XPNUtils: this.util.address,
     },
   });
-  // TODO: Change thsese for and underlying asset address
-  let mockAddress = "0x0000000000000000000000000000000000000000";
-  const assetAddresses = [
-    { name: "BTC", address: mockAddress, priceFeed: mockAddress },
-    { name: "ETH", address: mockAddress, priceFeed: mockAddress },
-    { name: "USDT", address: mockAddress, priceFeed: mockAddress },
+  const Signal = await ethers.getContractFactory("XPNSignal");
+  this.simpleSignal = await Signal.deploy();
+  await this.simpleSignal.deployed();
+
+  // TODO: Change signal name
+  await this.simpleSignal.registerSignal("testsignal1", "Simple", ["ETH"]);
+  await this.simpleSignal.submitSignal("testsignal1", ["ETH"], [1], "0x");
+
+  const contracts = {
+    ENZYME_DEPLOYER: {
+      address: "0x8375F0423C1DE0D8aCDCa79224F3d47fE8E6E197",
+    },
+    ENZYME_INT_MANAGER: {
+      address: "0x81198c4b6954CbA32d7aCE52580bd7a1265DEa3f",
+    },
+    ENZYME_ASSET_ADAPTER: {
+      address: "0x837d050F4a9eed50E32dAc9571Cf22F064514891",
+    },
+    ENZYME_POLICY_MANAGER: {
+      address: "0x49FA9A194D0fC7bbD8f350AF2206975EDB7E2c3b",
+    },
+    ENZYME_INVESTOR_WHITELIST: {
+      address: "0x9A300Ab621B41B7b8dC16775Ca1FDc9AB569C1B8",
+    },
+    WETH: {
+      address: "0xd0a1e359811322d97991e03f863a0c30c2cf029c",
+    },
+  };
+
+  const constructorArg = [
+    process.env.ADMIN_ADDRESS,
+    process.env.SETTLER_ADDRESS,
+    this.simpleSignal.address, // signal address
+    "ETH",
+    contracts.ENZYME_DEPLOYER.address,
+    contracts.ENZYME_INT_MANAGER.address,
+    contracts.ENZYME_ASSET_ADAPTER.address,
+    contracts.ENZYME_POLICY_MANAGER.address, // policy manager
+    contracts.ENZYME_INVESTOR_WHITELIST.address, // investor whitelist
+    ethers.constants.AddressZero,
+    ethers.constants.AddressZero,
+    [],
+    "EX-ETH",
   ];
-  signalFund = await SignalFund.deploy(
-    vault.address,
-    assetAddresses.map((x) => x.name),
-    assetAddresses.map((x) => x.address),
-    assetAddresses.map((x) => x.priceFeed)
+
+  this.core = await Core.deploy(
+    constructorArgs,
+    contracts.WETH.address,
+    "EX-ETH",
+    "EX-ETH"
   );
-  console.log("XPNSignalCore deployed to:", signalFund.address);
+  await this.core.deployed();
+  await this.core.setSignal(this.simpleSignal.address, "testsignal1");
 }
 
 main()
