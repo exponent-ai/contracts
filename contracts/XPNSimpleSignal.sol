713b2da0d6bbb850d510d4172102d150e98c7ca5
Ξ X A <83942971+exa256@users.noreply.github.com>
2021-09-21 13:31:21 +0700

    QSP-20 fix: remove (^) unlocked pragma from source files (#43)

--- contracts/XPNSimpleSignal.sol
+++ contracts/XPNSimpleSignal.sol
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Exponent.  If not, see <http://www.gnu.org/licenses/>.
 
-pragma solidity ^0.8.0;
+pragma solidity 0.8.0;
 
 import "./interface/ISignal.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
--- contracts/interface/IAssetManagerAdapter.sol
+++ contracts/interface/IAssetManagerAdapter.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+pragma solidity 0.8.0;
 
 interface IAssetManagerAdapter {
     // a trade adapter can be a mock, integrated with enzyme, or just paper trade
